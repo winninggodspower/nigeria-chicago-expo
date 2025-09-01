348dda7a8ff484cfdfa88d4b189cfe17d878fee3
winning godspower <winninggodspower@gmail.com>
2025-09-01 10:05:46 +0100

    feat: rebuild nextjs site

---
+++ .next/static/chunks/src_app_event_page_a65d8563.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_7fadc6f4._.js",
+  "static/chunks/node_modules_b095b892._.js"
+],
+    source: "dynamic"
+});
